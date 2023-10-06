2d6e8302d166be7daaa2f2ccd1712e54f030ba92
intrnl <intrnl28+git@gmail.com>
2023-10-06 09:04:56 +0700

    feat: add repeat function

--- src/public/index.ts
+++ src/public/index.ts
@@ -1,2 +1,15 @@
 export * from '../types.js';
 export { html } from '../runtime/index.js';
+
+export const repeat = <T, R>(array: T[], mapper: (value: T, idx: number, array: T[]) => R): R[] => {
+	let idx = 0;
+	let len = array.length;
+
+	let out = new Array(len);
+
+	for (; idx < len; idx++) {
+		out[idx] = mapper(array[idx], idx, array);
+	}
+
+	return out;
+};
